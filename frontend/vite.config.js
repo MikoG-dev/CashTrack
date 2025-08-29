b3161de1794e75fb823da01e1184da8a753ac51d
Milkiyas Weldesenbet <milkiyasweldesenbet@gmail.com>
2025-08-29 11:58:24 +0300

    update vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173
+  },
   // base: './',
   plugins: [react()],
 })
